1e24a0d576ab490ee486f30fbb23127d58fe0909
LeadConsult <leadconsultnig@gmail.com>
2022-11-12 04:46:03 +0100

    s

--- 101-locked_class.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" 
-LockedClass class """
-
-class LockedClass():
-    """ 
-    variables declarations """
-    __slots__ = ('first_name')
--- 19-copy_list.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def copy_list(my_List):
-    return my_List[:]
