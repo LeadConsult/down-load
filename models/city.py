043faca2a596106220b163f2a365ecea5a5d7d57
LeadConsult <leadconsultnig@gmail.com>
2023-01-07 03:29:58 +0100

    ttt

--- models/city.py
+++ models/city.py
@@ -1,12 +1,19 @@
-class State(BaseModel, Base):
-    """ The state class, contains a name and a list of cities """
-    # ...
+#!/usr/bin/python3
+""" City Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
-    @property
-    def cities(self):
-        from models.city import City
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
+        state_id = ''
