69279b78839cc4c90fdf99516337ce92bb03efd5
LeadConsult <leadconsultnig@gmail.com>
2023-03-31 08:40:22 +0100

    ttt

--- app.py
+++ app.py
@@ -1,32 +1,87 @@
-from flask import Flask, render_template
-import requests
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 
 app = Flask(__name__)
 app.debug = True
+app.config['SECRET_KEY'] = 'your?secret?key'
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
 
-def cryptocurrency_graphs():
-    # URLs to fetch cryptocurrency graph data
-    bitcoin_url = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=30'
-    ethereum_url = 'https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30'
-    cardano_url = 'https://api.coingecko.com/api/v3/coins/cardano/market_chart?vs_currency=usd&days=30'
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
 
-    # Fetch cryptocurrency graph data
-    bitcoin_response = requests.get(bitcoin_url)
-    bitcoin_data = bitcoin_response.json()
-    ethereum_response = requests.get(ethereum_url)
-    ethereum_data = ethereum_response.json()
-    cardano_response = requests.get(cardano_url)
-    cardano_data = cardano_response.json()
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
 
-    # Process cryptocurrency graph data
-    bitcoin_prices = bitcoin_data['prices']
-    bitcoin_timestamps = [price[0] for price in bitcoin_prices]
-    bitcoin_values = [price[1] for price in bitcoin_prices]
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
